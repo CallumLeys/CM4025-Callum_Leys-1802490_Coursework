32a0d20052423083ba2e2796c7c319681b89ac7f
callumleys <callumleys@icloud.com>
2023-04-10 22:21:05 +0100

    keys test

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://[MONGO_DETAILS]@cluster0.3oynuet.mongodb.net/?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://[MONGO_DETAILSs]@cluster0.3oynuet.mongodb.net/?retryWrites=true&w=majority',
     secretOrKey: "secret"
 };
+
+
