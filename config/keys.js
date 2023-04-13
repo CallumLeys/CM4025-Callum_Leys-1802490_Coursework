c684a1b2601dbe5a6795706510a154c9ee004862
callumleys <callumleys@icloud.com>
2023-04-13 15:10:18 +0100

    keys update

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://cleys:mupI4IAcVOFQCXjH@cluster0.3oynuet.mongodb.net/?retryWrites=true&w=majority',
+    mongoURI: '[Enter URI]',
     secretOrKey: "secret"
 };
